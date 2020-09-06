02661a18bfc3702ee785f3f8f090ef1e017198fe
rajarahul12 <maddula.rajarahul@gmail.com>
2020-09-06 14:55:18 +0530

    UI Improvements

--- src/InputForm.js
+++ src/InputForm.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
-import { SearchOutlined } from "@material-ui/icons";
+import AttachmentIcon from "@material-ui/icons/Attachment";
+import SendIcon from "@material-ui/icons/Send";
+import { Button, CircularProgress } from "@material-ui/core";
 
 import "./InputForm.css";
+import axios from "./axios";
 
 function InputForm() {
   const [longURL, setLongURL] = useState("");
@@ -11,32 +14,36 @@ function InputForm() {
     setLongURL(e.target.value);
   };
 
-  const handleClick = () => {
+  const handleClick = (e) => {
+    e.preventDefault();
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
     var raw = JSON.stringify({ longUrl: longURL });
 
-    var requestOptions = {
-      method: "POST",
-      headers: myHeaders,
-      body: raw,
-      mode: "cors",
+    var config = {
+      method: "post",
+      url: "/api/url/shorten",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: raw,
     };
 
-    fetch("http://localhost:5000/api/url/shorten", requestOptions)
-      .then((response) => response.json())
-      .then((result) => {
-        setShortURL(result.shortUrl);
+    axios(config)
+      .then(function (response) {
+        setShortURL(response.data.shortUrl);
       })
-      .catch((error) => console.log("error", error));
+      .catch(function (error) {
+        console.log(error);
+      });
   };
 
   return (
     <div className="inputform">
-      <div className="inputform__userInput">
+      <form className="inputform__userInput">
         <div className="inputform__longurl">
-          <SearchOutlined />
+          <AttachmentIcon />
           <input
             value={longURL}
             onChange={handleURLChange}
@@ -45,9 +52,21 @@ function InputForm() {
           />
         </div>
 
-        <button onClick={handleClick}>Shorten</button>
-      </div>
+        <Button
+          size="medium"
+          endIcon={<SendIcon />}
+          onClick={handleClick}
+          variant="contained"
+          color="primary"
+          type="submit"
+        >
+          Shorten
+        </Button>
+        {/* <CircularProgress color="primary" size={30} /> */}
+      </form>
+
       <div className="inputform__shorturl">
+        <AttachmentIcon />
         <input value={shortURL} placeholder="Short link" type="text" />
       </div>
     </div>
